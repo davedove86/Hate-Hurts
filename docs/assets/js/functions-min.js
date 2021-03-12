adb9575246a445199bcb1baefcca46d5099e224e
davedove86 <davedove@icloud.com>
2021-03-12 07:36:51 +0000

    Webflow code

--- docs/assets/js/functions-min.js
+++
@@ -1,1 +0,0 @@
-const navSlide=()=>{const e=document.querySelector(".burger"),n=document.querySelector(".nav-links"),t=document.querySelectorAll(".nav-links li");e.addEventListener("click",(()=>{n.classList.toggle("nav-active"),t.forEach(((e,n)=>{e.style.animation?e.style.animation="":e.style.animation=`navLinkFade 0.5s ease forwards ${n/7+.3}s`})),e.classList.toggle("toggle")}))};navSlide();
